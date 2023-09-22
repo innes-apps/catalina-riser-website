7540b17abc21c3690d58094c19c99d8b08acbea3
Jeff Innes <contact.me@jeffinnes.dev>
2023-09-22 13:35:14 -0700

    fix favicon

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -11,7 +11,7 @@ export default function About() {
         <title>Catalina Riser Yoga</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <div className={styles.aboutContent}>
