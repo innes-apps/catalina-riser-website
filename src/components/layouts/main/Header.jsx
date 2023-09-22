7290e003b86e3c19d2567d189549c272d5645afb
Jeff Innes <contact.me@jeffinnes.dev>
2023-09-21 21:36:54 -0700

    Style header for desktop and add mobile header

--- src/components/layouts/main/Header.jsx
+++ src/components/layouts/main/Header.jsx
@@ -1,22 +1,46 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link';
 import Image from 'next/image';
-import { lato } from '@/utils/font-loader';
+import { lato, rofane } from '@/utils/font-loader';
 
 import styles from './MainLayout.module.css';
 
 export default function Header() {
   return (
-    <header className={`${styles.header} ${lato.className}`}>
-      <nav className={styles.leftNav} aria-label="left of logo nav">
-        <Link href="/about">About</Link>
-      </nav>
+    <>
+      <header className={`${styles.header} ${rofane.className}`}>
+        <nav className={styles.leftNav} aria-label="left of logo nav">
+          <Link href="/about">About</Link>
+        </nav>
 
-      <img src={'https://picsum.photos/150'} alt="logo" className={styles.logo} />
+        <Image
+          src={'/img/Logo Circle_transp.png'}
+          alt="Catalina Riser Yoga Logo"
+          width={150}
+          height={150}
+          className={styles.logo}
+        />
 
-      <nav className={styles.rightNav} aria-label="right of logo nav">
-        <Link href="/contact">Contact</Link>
-      </nav>
-    </header>
+        <nav className={styles.rightNav} aria-label="right of logo nav">
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </header>
+      <header className={`${styles.mobileHeader} ${rofane.className}`}>
+        <div className={styles.mobileBanner}>
+          <Image
+            src={'/img/Logo_Full.png'}
+            alt="Catalina Riser Yoga Logo"
+            width={257}
+            height={150}
+            className={styles.logo}
+          />
+        </div>
+
+        <nav className={styles.mobileNav}>
+          <Link href="/about">About</Link>
+          <Link href="/contact">Contact</Link>
+        </nav>
+      </header>
+    </>
   );
 }
