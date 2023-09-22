b0f0515f326baccca8f8cb13af7488cbf0cdaf24
Jeff Innes <contact.me@jeffinnes.dev>
2023-09-21 21:35:50 -0700

    Add Rofane font to about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Head from 'next/head';
 import Link from 'next/link';
-
+import { rofane } from '@/utils/font-loader';
 import styles from '@/styles/About.module.css';
 
 export default function About() {
@@ -20,7 +20,7 @@ export default function About() {
             <img src="https://picsum.photos/300?random=1&grayscale" />
           </div>
           <div className={styles.sectionContent}>
-            <h2>Personal Story pt. 1</h2>
+            <h2 className={rofane.className}>Personal Story pt. 1</h2>
             <p className={styles.copy}>
               Katie Innes is a certified yoga instructor based in Tucson, Arizona. She has been
               practicing yoga for over 10 years and has been teaching for 3 years. She is a 200 hour
@@ -37,7 +37,7 @@ export default function About() {
             <img src="https://picsum.photos/300?random=2" />
           </div>
           <div className={styles.sectionContent}>
-            <h2>Personal Story pt. 2</h2>
+            <h2 className={rofane.className}>Personal Story pt. 2</h2>
             <p className={styles.copy}>
               Katie Innes is a certified yoga instructor based in Tucson, Arizona. She has been
               practicing yoga for over 10 years and has been teaching for 3 years. She is a 200 hour
@@ -54,7 +54,7 @@ export default function About() {
             <img src="https://picsum.photos/300?random=3&blur=3" />
           </div>
           <div className={styles.sectionContent}>
-            <h2>Philosophy and Offerings</h2>
+            <h2 className={rofane.className}>Philosophy and Offerings</h2>
             <p className={styles.copy}>
               Katie Innes is a certified yoga instructor based in Tucson, Arizona. She has been
               practicing yoga for over 10 years and has been teaching for 3 years. She is a 200 hour
