9dcfb873781d8499de7820a462a3a831806ac065
Jeff Innes <contact.me@jeffinnes.dev>
2023-09-23 13:04:14 -0700

    Add Fontawesome
    All of the free font awesome for now. I'll tree shake this later

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,8 @@
 import '@/styles/globals.css';
 import '@/styles/reset.css';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 import Layout from '@/components/layouts/main/Layout';
 
