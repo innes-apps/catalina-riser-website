e2253c8b0ae5cd816746a5bb005b314a5d15bd80
Jeff Innes <contact.me@jeffinnes.dev>
2024-04-13 22:15:00 -0700

    Add logic to gracefully handle empty days

--- src/components/classSchedule/ClassList.tsx
+++ src/components/classSchedule/ClassList.tsx
@@ -19,24 +19,28 @@ export interface ClassListProps {
 }
 
 const ClassList: React.FC<ClassListProps> = ({ classes }) => {
-  return (
-    <ul className={styles.classList}>
-      {classes.map((item) => (
-        <li key={item._id} className={styles.classListItem}>
-          <p>
-            {item.title} {item.time}
-          </p>
-          <p>{item.location}</p>
-          {item.signUpLink && (
-            <Link href={item.signUpLink} target="_blank" rel="noopener noreferrer">
-              Sign Up <FontAwesomeIcon icon={faUpRightFromSquare} size="sm" />
-            </Link>
-          )}
-          {item.note && <p>{item.note}</p>}
-        </li>
-      ))}
-    </ul>
-  );
+  if (classes) {
+    return (
+      <ul className={styles.classList}>
+        {classes.map((item) => (
+          <li key={item._id} className={styles.classListItem}>
+            <p>
+              {item.title} {item.time}
+            </p>
+            <p>{item.location}</p>
+            {item.signUpLink && (
+              <Link href={item.signUpLink} target="_blank" rel="noopener noreferrer">
+                Sign Up <FontAwesomeIcon icon={faUpRightFromSquare} size="sm" />
+              </Link>
+            )}
+            {item.note && <p>{item.note}</p>}
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default ClassList;
