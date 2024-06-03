42291c45804cce37e9a2e156185db9d1bbb1e1e1
Jeff Innes <contact.me@jeffinnes.dev>
2024-06-03 14:28:50 -0700

    Initial desktop styling is done. Committing with placeholder data to demo for client.

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -27,29 +27,37 @@ export default function Events(props: { events: any[] }) {
             const date = new Date(event.date);
 
             return (
-              <div key={event._id}>
+              <div key={event._id} className={styles.event}>
                 <h3>{event.title}</h3>
-                <div className={styles.eventCardContent}>
-                  <div className={styles.eventInfo}>
+                <div className={styles.eventDetails}>
+                  <div className={`${styles.eventInfo} ${styles.eventDate}`}>
                     <h4>Date</h4>
                     <p>
                       {/* See above for explaination of why we use UTC */}
                       {date.getUTCMonth() + 1}/{date.getUTCDate()}/{date.getUTCFullYear()}
                     </p>
                   </div>
-                  <div className={styles.eventInfo}>
+
+                  <div className={`${styles.eventInfo} ${styles.eventTime}`}>
                     <h4>Time</h4>
                     <p>{event.time}</p>
                   </div>
-                  <div className={styles.eventInfo}>
+
+                  <div className={`${styles.eventInfo} ${styles.eventLocation}`}>
                     <h4>Location</h4>
                     <p>{event.location}</p>
                   </div>
+
+                  <div className={`${styles.eventInfo} ${styles.eventDescription}`}>
+                    <h4>Description</h4>
+                    <p>{event.description}</p>
+                  </div>
+
                   <Link
                     href={event.signUpLink}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="linkAsButton"
+                    className={`linkAsButton ${styles.eventSignUpLink}`}
                   >
                     <FontAwesomeIcon icon={faLink} size="sm" /> Sign Up!
                   </Link>
@@ -69,7 +77,64 @@ export async function getStaticProps() {
     const eventQuery = `*[_type == "events"] | order(date desc) { _id, title, description, date, time, location, signUpLink }`;
     const events = await client.fetch(eventQuery);
 
-    return { props: { events }, revalidate: 60 };
+    // ToDo replace with real data before deploying
+    return {
+      props: {
+        events: [
+          {
+            _id: 'event1',
+            title: 'Yoga for Beginners',
+            description:
+              "Discover the gentle art of yoga, a practice that awakens body, mind, and spirit. In this class, you'll learn basic poses and breathing techniques to set the foundation for your yoga journey.",
+            date: '2023-01-01',
+            time: '12:00PM - 3:00PM',
+            location: '123 Main St.',
+            signUpLink: 'https://example.com/signup',
+          },
+          {
+            _id: 'event2',
+            title: 'Strength Yoga',
+            description:
+              "Build your strength and flexibility with this challenging class. You'll be pushed to your limits as you explore dynamic flow sequences that engage your muscles from head to toe.",
+            date: '2023-01-08',
+            time: '6:00PM - 9:00PM',
+            location: '456 Elm St.',
+            signUpLink: 'https://example.com/signup',
+          },
+          {
+            _id: 'event3',
+            title: 'Restorative Yoga',
+            description:
+              'Find peace and relaxation in this deeply nurturing practice. Let go of tension and let your body rejuvenate as you settle into gentle restorative poses that invigorate the spirit and calm the mind.',
+            date: '2023-01-15',
+            time: '10:00AM - 5:00PM',
+            location: '789 Oak St.',
+            signUpLink: 'https://example.com/signup',
+          },
+          {
+            _id: 'event4',
+            title: 'Yoga for Arthritis',
+            description:
+              'A gentle practice designed to alleviate joint pain. This class will guide you through sequences that target specific joints, providing a safe and effective way to reduce stress and tension.',
+            date: '2023-01-22',
+            time: '7:00PM - 9:00PM',
+            location: '321 Maple St.',
+            signUpLink: 'https://example.com/signup',
+          },
+          {
+            _id: 'event5',
+            title: 'Yin Yoga',
+            description:
+              "Experience a deep release of tension and stress with this slow-flowing practice. Through a series of gentle postures and breathing techniques, you'll find your body and mind rejuvenated and relaxed.",
+            date: '2023-01-29',
+            time: '9:00AM - 11:00AM',
+            location: '567 Pine St.',
+            signUpLink: 'https://example.com/signup',
+          },
+        ],
+      },
+      revalidate: 60,
+    };
   } catch (error) {
     Sentry.captureException(error);
     return { props: {} };
