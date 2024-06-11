bdb2baa975600f1fa38b5cb1be7832251fd8980c
Jeff Innes <contact.me@jeffinnes.dev>
2024-06-10 21:39:06 -0700

    Modify Sanity config to get dataset value from env

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity';
 
 export const client = createClient({
   projectId: 'vbqy41y7',
-  dataset: 'production',
+  dataset: process.env.SANITY_DATASET,
   apiVersion: '2024-01-01',
   useCdn: true,
 });
