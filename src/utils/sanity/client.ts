a4670de42c408591ba59247fcc0e5a53f28fb5ce
Jeff Innes <contact.me@jeffinnes.dev>
2025-01-07 22:10:38 -0700

    Update Sanity CDN usage condition based on environment variable

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -4,5 +4,5 @@ export const client = createClient({
   projectId: 'vbqy41y7',
   dataset: process.env.SANITY_DATASET,
   apiVersion: '2024-01-01',
-  useCdn: true,
+  useCdn: process.env.SANITY_DATASET === 'production' ? true : false,
 });
